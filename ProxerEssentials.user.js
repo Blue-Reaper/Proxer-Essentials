234487000a8dfb3567fb6003ecd35a8b5f8e696a
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2019-01-22 20:13:34 +0100

    change version

--- ProxerEssentials.user.js
+++ ProxerEssentials.user.js
@@ -1,7 +1,7 @@
 "use strict";
 // ==UserScript==
 // @name        Proxer Essentials
-// @version     4.4.1-beta
+// @version     4.4-beta.1
 // @description Nützlicher Erweiterungen für Proxer die jeder haben sollte.
 // @author      Blue.Reaper
 // @namespace   https://blue-reaper.github.io/Proxer-Essentials/
