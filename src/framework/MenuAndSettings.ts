d7e35383619f0631ca464d42c5fe0d4a484df50f
xAzzzu <constantincuy@gmail.com>
2019-01-25 19:26:31 +0100

    Added package.json for easier builds. Updated some code to make it work via the new build functionality.

--- src/framework/MenuAndSettings.ts
+++ src/framework/MenuAndSettings.ts
@@ -1,101 +1,120 @@
 //Fügt den Button "Essentials" zu "leftNav" hinzu
 // Erzeugt 'Einstellungen' in Essentials
 function addPefMenu() {
-	$('#leftNav').append($('<li class="topmenu"><a href="javascript:;">Essentials ▾</a><ul id="pef_menu"></ul></li>'));
-	// Erzeugt 'Einstellungen' in Essentials
-	$('#pef_menu').append($('<li><a href="/pef?s=settings#top">Einstellungen</a></li>'));
-};
+    $('#leftNav').append(
+        $(
+            '<li class="topmenu"><a href="javascript:;">Essentials ▾</a><ul id="pef_menu"></ul></li>'
+        )
+    );
+    // Erzeugt 'Einstellungen' in Essentials
+    $('#pef_menu').append(
+        $('<li><a href="/pef?s=settings#top">Einstellungen</a></li>')
+    );
+}
 
 // Zeigt die Settings des PEF an
 // Erzeugt die Einstellungs-Seite für PEF
 // Da es proxer.me/pef nicht gibt, wird die Startseite angezeigt
-function createPefSettings(){
-	if(window.location.pathname.split('/')[1] === 'pef'){
-		// // Lösche alle Tabs der Startseite aus Navigations-Leiste
-		$('#simple-navi').empty();
-// 		Lösche den Inhalt der Seite
-		$('div.inner').empty();
-		let inhalt = $('div.inner');
+function createPefSettings() {
+    if (window.location.pathname.split('/')[1] === 'pef') {
+        // // Lösche alle Tabs der Startseite aus Navigations-Leiste
+        $('#simple-navi').empty();
+        // 		Lösche den Inhalt der Seite
+        $('div.inner').empty();
+        let inhalt = $('div.inner');
 
-		// Setze den Titel des Tabs im Browser
-		document.title = 'Proxer Essentials';
+        // Setze den Titel des Tabs im Browser
+        document.title = 'Proxer Essentials';
 
-// 		Erzeuge Tab Einstellungen
-// 		Id: pef_Settings
-// 		URL: ?s=settings
-		$('#simple-navi').append($('<li id ="pef_Settings"><a data-ajax="true" href="/pef?s=settings#top">Einstellungen</a></li>'));
+        // 		Erzeuge Tab Einstellungen
+        // 		Id: pef_Settings
+        // 		URL: ?s=settings
+        $('#simple-navi').append(
+            $(
+                '<li id ="pef_Settings"><a data-ajax="true" href="/pef?s=settings#top">Einstellungen</a></li>'
+            )
+        );
 
-		// Erzeugt den Ihalt des Tabs 'Einstellungen'
-		if(location.search === "" || location.search === "?s=settings"){
-			$('#pef_Settings').addClass("active");
-			document.title = 'Proxer Essentials';
+        // Erzeugt den Ihalt des Tabs 'Einstellungen'
+        if (location.search === '' || location.search === '?s=settings') {
+            $('#pef_Settings').addClass('active');
+            document.title = 'Proxer Essentials';
 
-			// Überschrift
-			inhalt.append($('<h3 class="floatLeft">Proxer Essentials</h3>'));
-			inhalt.append($('<div class="floatLeft version">'+GM_info.script.version+'</div>'));
+            // Überschrift
+            inhalt.append($('<h3 class="floatLeft">Proxer Essentials</h3>'));
+            inhalt.append(
+                $('<div class="floatLeft version">' + GM_info.script.version + '</div>')
+            );
 
-			// Inhalt für Modulanzeige
-			let pef_module = $('<div class="clear"/>');
-			inhalt.append(pef_module);
-			showModules(pef_module);
+            // Inhalt für Modulanzeige
+            let pef_module = $('<div class="clear"/>');
+            inhalt.append(pef_module);
+            showModules(pef_module);
 
-			// Footer
-			let designLink = $('<a href="javascript:;">Design by xYata</a>');
-			let footer = $('<div class ="clear"></div>');
-			footer.append(designLink);
-			inhalt.append(footer);
+            // Footer
+            let designLink = $('<a href="javascript:;">Design by xYata</a>');
+            let footer = $('<div class ="clear"></div>');
+            footer.append(designLink);
+            inhalt.append(footer);
 
-			designLink.click(()=>{
-				if (GM_getValue("ModernDarkStatus") === "off") {
-					GM_setValue("ModernDarkStatus","on");
-				} else {
-					GM_setValue("ModernDarkStatus","off");
-				}
-				location.reload();
-			});
-		}
-	}
+            designLink.click(() => {
+                if (GM_getValue('ModernDarkStatus') === 'off') {
+                    GM_setValue('ModernDarkStatus', 'on');
+                } else {
+                    GM_setValue('ModernDarkStatus', 'off');
+                }
+                location.reload();
+            });
+        }
+    }
 }
 
 // Zeitgt die einzelnen Module auf der Einstellungs-Seite an
-function showModules(pef_module){
+function showModules(pef_module: any) {
+    // Fügt jedes Modul hinzu
+    for (let singleModule of pefModulList) {
+        let moduleBox = $(
+            '<div id="' +
+            singleModule.id +
+            'ModulBox" class="floatLeft modulBox"></div>'
+        );
 
-// Fügt jedes Modul hinzu
-	for(let singleModule of pefModulList){
-		let moduleBox = $('<div id="'+singleModule.id+'ModulBox" class="floatLeft modulBox"></div>');
+        moduleBox.append($('<h3>' + singleModule.name + '</h3>'));
 
-		moduleBox.append($('<h3>'+singleModule.name+'</h3>'));
+        moduleBox.append($('<div>' + singleModule.description + '</div>'));
+        moduleBox.append(
+            $('<div class="autor">by ' + singleModule.autor + '</div>')
+        );
+        // IDEA: Button für Details hinzufügen
+        let modulStatus = $(
+            '<i id="' + singleModule.id + '_StatusImg" class="fa fa-2x pointer"/>'
+        );
+        moduleBox.append(modulStatus);
 
-		moduleBox.append($('<div>'+singleModule.description+'</div>'));
-		moduleBox.append($('<div class="autor">by '+singleModule.autor+'</div>'));
-		// IDEA: Button für Details hinzufügen
-		let modulStatus = $('<i id="'+singleModule.id+'_StatusImg" class="fa fa-2x pointer"/>');
-		moduleBox.append(modulStatus);
+        pef_module.append(moduleBox);
+        updateModulTick(singleModule.id);
 
-		pef_module.append(moduleBox);
-		updateModulTick(singleModule.id);
-
-		modulStatus.click(()=>toggleModulStatus(singleModule));
-	}
+        modulStatus.click(() => toggleModulStatus(singleModule));
+    }
 }
 
 // Troogelt den Speicherwert und ruft das Modul auf
-function toggleModulStatus(modul :IPefModul) {
-	if (GM_getValue(modul.id+"Status") === "off") {
-		GM_setValue(modul.id+"Status","on");
-		actionControl(ModulCallEvent.on, modul);
-	} else {
-		GM_setValue(modul.id+"Status","off");
-		actionControl(ModulCallEvent.off, modul);
-	}
+function toggleModulStatus(modul: IPefModul) {
+    if (GM_getValue(modul.id + 'Status') === 'off') {
+        GM_setValue(modul.id + 'Status', 'on');
+        actionControl(ModulCallEvent.on, modul);
+    } else {
+        GM_setValue(modul.id + 'Status', 'off');
+        actionControl(ModulCallEvent.off, modul);
+    }
     updateModulTick(modul.id);
-};
+}
 
 // Setzt den Haken / Kreuz nach dem Modulnamen
-function updateModulTick(modulId :string) {
-    if (GM_getValue(modulId+"Status") === "off") {
-		$("#"+modulId+"ModulBox").removeClass('active');
-	} else {
-		$("#"+modulId+"ModulBox").addClass('active');
-	}
-};
+function updateModulTick(modulId: string) {
+    if (GM_getValue(modulId + 'Status') === 'off') {
+        $('#' + modulId + 'ModulBox').removeClass('active');
+    } else {
+        $('#' + modulId + 'ModulBox').addClass('active');
+    }
+}
--- src/modules/MangaComfort.ts
+++ src/modules/MangaComfort.ts
@@ -3,61 +3,74 @@
 // Longstrtip: letzte Bild springt in nächste Kapitel (ohne Zwischenseite)
 
 pefModulList.push({
-    id:"mangaComfort",
-    name:"Manga Comfort",
-    description:"mehr Komfort beim Manga Lesen",
-    autor:"Blue.Reaper",
-	callMethod:(change)=>mangaComfortCall(change)
+    id: 'mangaComfort',
+    name: 'Manga Comfort',
+    description: 'mehr Komfort beim Manga Lesen',
+    autor: 'Blue.Reaper',
+    callMethod: change => mangaComfortCall(change)
 });
 
-function mangaComfortCall (change:ModulCallEvent) {
-	switch(change) {
-		case ModulCallEvent.on:
-			mangaComfort();
-			break;
-		case ModulCallEvent.off:
-			break;
-		case ModulCallEvent.ajax:
+function mangaComfortCall(change: ModulCallEvent) {
+    switch (change) {
+        case ModulCallEvent.on:
             mangaComfort();
-			break;
-	}
+            break;
+        case ModulCallEvent.off:
+            break;
+        case ModulCallEvent.ajax:
+            mangaComfort();
+            break;
+    }
 }
 
-function mangaComfort(){
-
-	if (window.location.pathname.split('/')[1] !== 'read' && window.location.pathname.split('/')[1] !== 'chapter'){
-		return;
-	}
+function mangaComfort() {
+    if (
+        window.location.pathname.split('/')[1] !== 'read' &&
+        window.location.pathname.split('/')[1] !== 'chapter'
+    ) {
+        return;
+    }
 
     // Setzt Longstrip als Standard, wenn noch kein Cookie gesetzt ist
-    if(getCookie("manga_reader") != "slide"){
-        setCookie('manga_reader','longstrip');
+    if (getCookie('manga_reader') != 'slide') {
+        setCookie('manga_reader', 'longstrip');
     }
 
     // Ändere Link auf Bildern, damit nur zum nächsten Bild gesprungen wird
-    if(getCookie("manga_reader") == "longstrip"){
-        $('#reader img').attr("onclick","");
-        $('#reader img').off("click", scrollToNextPage);
+    if (getCookie('manga_reader') == 'longstrip') {
+        $('#reader img').attr('onclick', '');
+        $('#reader img').off('click', scrollToNextPage);
         $('#reader img').click(scrollToNextPage);
-        $('#reader img:last-child').attr("onclick","window.location=nextChapter.replace('chapter','read')+'#top'");
+        $('#reader img:last-child').attr(
+            'onclick',
+            "window.location=nextChapter.replace('chapter','read')+'#top'"
+        );
     }
 
     // Wenn 404, dann nächste Kapitel nicht verfügbar (Sprung direkt in nächste Kapitel) -> gehe zurück auf Zwichenseite
-    if($('#main img[src="/images/misc/404.png"]').length){
-        window.location.pathname=window.location.pathname.replace('read','chapter');
+    if ($('#main img[src="/images/misc/404.png"]').length) {
+        window.location.pathname = window.location.pathname.replace(
+            'read',
+            'chapter'
+        );
     }
 
     // Mauszeiger wird auch nur über Bild zur Hand
-    $('#reader img').addClass("pointer");
-    $('#reader a').addClass("cursorAuto");
-
-
+    $('#reader img').addClass('pointer');
+    $('#reader a').addClass('cursorAuto');
 }
 
-function scrollToNextPage(){
-    $('body,html').animate({
-        // nicht page+1, da id bei 0 los zählt
-        scrollTop: $('#chapterImage'+(current_page)).offset().top
-    }, 800);
-    current_page = current_page+1;
+function scrollToNextPage() {
+    // @ts-ignore
+    const image = $('#chapterImage' + current_page).offset();
+
+    $('body,html').animate(
+        {
+            // nicht page+1, da id bei 0 los zählt
+            scrollTop: image ? image.top : 0
+        },
+        800
+    );
+    // @ts-ignore
+    current_page = current_page + 1;
 }
