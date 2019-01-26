9356d5b43152cd9693c00af7d2e2d6db34831ddc
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2019-01-26 12:44:06 +0100

    compile changes

--- ProxerEssentials.user.js
+++ ProxerEssentials.user.js
@@ -58,7 +58,6 @@ function addPefMenu() {
     // Erzeugt 'Einstellungen' in Essentials
     $('#pef_menu').append($('<li><a href="/pef?s=settings#top">Einstellungen</a></li>'));
 }
-;
 // Zeigt die Settings des PEF an
 // Erzeugt die Einstellungs-Seite für PEF
 // Da es proxer.me/pef nicht gibt, wird die Startseite angezeigt
@@ -76,8 +75,8 @@ function createPefSettings() {
         // 		URL: ?s=settings
         $('#simple-navi').append($('<li id ="pef_Settings"><a data-ajax="true" href="/pef?s=settings#top">Einstellungen</a></li>'));
         // Erzeugt den Ihalt des Tabs 'Einstellungen'
-        if (location.search === "" || location.search === "?s=settings") {
-            $('#pef_Settings').addClass("active");
+        if (location.search === '' || location.search === '?s=settings') {
+            $('#pef_Settings').addClass('active');
             document.title = 'Proxer Essentials';
             // Überschrift
             inhalt.append($('<h3 class="floatLeft">Proxer Essentials</h3>'));
@@ -92,11 +91,11 @@ function createPefSettings() {
             footer.append(designLink);
             inhalt.append(footer);
             designLink.click(function () {
-                if (GM_getValue("ModernDarkStatus") === "off") {
-                    GM_setValue("ModernDarkStatus", "on");
+                if (GM_getValue('ModernDarkStatus') === 'off') {
+                    GM_setValue('ModernDarkStatus', 'on');
                 }
                 else {
-                    GM_setValue("ModernDarkStatus", "off");
+                    GM_setValue('ModernDarkStatus', 'off');
                 }
                 location.reload();
             });
@@ -106,7 +105,9 @@ function createPefSettings() {
 // Zeitgt die einzelnen Module auf der Einstellungs-Seite an
 function showModules(pef_module) {
     var _loop_1 = function (singleModule) {
-        var moduleBox = $('<div id="' + singleModule.id + 'ModulBox" class="floatLeft modulBox"></div>');
+        var moduleBox = $('<div id="' +
+            singleModule.id +
+            'ModulBox" class="floatLeft modulBox"></div>');
         moduleBox.append($('<h3>' + singleModule.name + '</h3>'));
         moduleBox.append($('<div>' + singleModule.description + '</div>'));
         moduleBox.append($('<div class="autor">by ' + singleModule.autor + '</div>'));
@@ -125,27 +126,25 @@ function showModules(pef_module) {
 }
 // Troogelt den Speicherwert und ruft das Modul auf
 function toggleModulStatus(modul) {
-    if (GM_getValue(modul.id + "Status") === "off") {
-        GM_setValue(modul.id + "Status", "on");
+    if (GM_getValue(modul.id + 'Status') === 'off') {
+        GM_setValue(modul.id + 'Status', 'on');
         actionControl(0 /* on */, modul);
     }
     else {
-        GM_setValue(modul.id + "Status", "off");
+        GM_setValue(modul.id + 'Status', 'off');
         actionControl(1 /* off */, modul);
     }
     updateModulTick(modul.id);
 }
-;
 // Setzt den Haken / Kreuz nach dem Modulnamen
 function updateModulTick(modulId) {
-    if (GM_getValue(modulId + "Status") === "off") {
-        $("#" + modulId + "ModulBox").removeClass('active');
+    if (GM_getValue(modulId + 'Status') === 'off') {
+        $('#' + modulId + 'ModulBox').removeClass('active');
     }
     else {
-        $("#" + modulId + "ModulBox").addClass('active');
+        $('#' + modulId + 'ModulBox').addClass('active');
     }
 }
-;
 //############################# Erstellen eines Dialogs #############################
 /*	Erzeugt einen Dialog
 Variante 1: ConfirmDialog
@@ -155,35 +154,35 @@ Variante 2: AlertDialog
 */
 function createPefDialog(msg, methodYes, methodNo) {
     // Testet, ob ein Confirm-, oder ein AlertDialog angezeigt werden soll
-    var confirmDialog = (methodYes != null && methodNo != null) ? true : false;
+    var confirmDialog = methodYes != null && methodNo != null ? true : false;
     // Damit der Hintergurnd gesperrt wird
-    var dialogoverlay = document.createElement("div");
-    dialogoverlay.className = "dialogOverlay";
+    var dialogoverlay = document.createElement('div');
+    dialogoverlay.className = 'dialogOverlay';
     // Das Dialogfeld
-    var dialogbox = document.createElement("div");
-    dialogbox.className = "message dialogBox";
+    var dialogbox = document.createElement('div');
+    dialogbox.className = 'message dialogBox';
     // Die Angezeigte Nachricht
-    var dialogmessage = document.createElement("div");
+    var dialogmessage = document.createElement('div');
     dialogmessage.innerHTML = msg;
     // Die Antwortbutton
-    var dialogbuttons = document.createElement("div");
+    var dialogbuttons = document.createElement('div');
     if (confirmDialog) {
-        dialogbuttons.className = "dialogYesNo";
+        dialogbuttons.className = 'dialogYesNo';
     }
     else {
-        dialogbuttons.className = "center dialogOk";
+        dialogbuttons.className = 'center dialogOk';
     }
-    var dialogbuttonyes = document.createElement("i");
-    dialogbuttonyes.className = "fa fa-check fa-2x pointer";
+    var dialogbuttonyes = document.createElement('i');
+    dialogbuttonyes.className = 'fa fa-check fa-2x pointer';
     dialogbuttons.appendChild(dialogbuttonyes);
     if (confirmDialog) {
-        var dialogbuttonno = document.createElement("i");
-        dialogbuttonno.className = "dialogButtonNo fa fa-times fa-2x pointer";
+        var dialogbuttonno = document.createElement('i');
+        dialogbuttonno.className = 'dialogButtonNo fa fa-times fa-2x pointer';
         dialogbuttons.appendChild(dialogbuttonno);
         $(dialogbuttonno).click(function () {
             messages.removeChild(dialogoverlay);
             messages.removeChild(dialogbox);
-            methodNo();
+            methodNo && methodNo();
         });
     }
     // Hinzufügen der Elemente
@@ -193,23 +192,23 @@ function createPefDialog(msg, methodYes, methodNo) {
     messages.appendChild(dialogoverlay);
     messages.appendChild(dialogbox);
     // Dialog mittig setzen
-    dialogbox.style.marginTop = (dialogbox.clientHeight / -2) + "px";
-    dialogbox.style.marginLeft = (dialogbox.clientWidth / -2) + "px";
+    dialogbox.style.marginTop = dialogbox.clientHeight / -2 + 'px';
+    dialogbox.style.marginLeft = dialogbox.clientWidth / -2 + 'px';
     // Klicken der Buttons löscht Dialog und ruft jeweilige Mothode auf
     $(dialogbuttonyes).click(function () {
         messages.removeChild(dialogoverlay);
         messages.removeChild(dialogbox);
         if (confirmDialog) {
-            methodYes();
+            methodYes && methodYes();
         }
     });
 }
-;
 //############################# Erstellen einer Message #############################
 //	Erzeugt eine Message
 function createPefMessage(msg) {
     // Proxer eigene Funktion
-    if (window.location.hostname !== "stream.proxer.me") {
+    if (window.location.hostname !== 'stream.proxer.me') {
+        // @ts-ignore
         create_message('key_suggestion', 7000, msg);
     }
 }
@@ -217,14 +216,17 @@ function createPefMessage(msg) {
 // Gibt den Wert des übergebenen Coockienamens wieder
 function getCookie(name) {
     // Proxer eigene Funktion
-    if (window.location.hostname !== "stream.proxer.me") {
+    if (window.location.hostname !== 'stream.proxer.me') {
+        // @ts-ignore
         return get_cookie(name);
     }
+    return '';
 }
 // Setzt ein Cookie
 function setCookie(name, value) {
     // Proxer eigene Funktion
-    if (window.location.hostname !== "stream.proxer.me") {
+    if (window.location.hostname !== 'stream.proxer.me') {
+        // @ts-ignore
         set_cookie(name, value, cookie_expire);
     }
 }
@@ -258,10 +260,10 @@ function actionControl(change, modul) {
 // Longstrtip: letzte Bild springt in nächste Kapitel (ohne Zwischenseite)
 // Fügt Mangaupdates neben Animeupdates auf Startseite hinzu
 pefModulList.push({
-    id: "mangaComfort",
-    name: "Manga Comfort",
-    description: "mehr Komfort beim Manga Lesen",
-    autor: "Blue.Reaper",
+    id: 'mangaComfort',
+    name: 'Manga Comfort',
+    description: 'mehr Komfort beim Manga Lesen',
+    autor: 'Blue.Reaper',
     callMethod: function (change) { return mangaComfortCall(change); }
 });
 function mangaComfortCall(change) {
@@ -286,29 +288,32 @@ function mangaComfort() {
         return;
     }
     // Setzt Longstrip als Standard, wenn noch kein Cookie gesetzt ist
-    if (getCookie("manga_reader") != "slide") {
+    if (getCookie('manga_reader') != 'slide') {
         setCookie('manga_reader', 'longstrip');
     }
     // Ändere Link auf Bildern, damit nur zum nächsten Bild gesprungen wird
-    if (getCookie("manga_reader") == "longstrip") {
-        $('#reader img').attr("onclick", "");
-        $('#reader img').off("click", scrollToNextPage);
+    if (getCookie('manga_reader') == 'longstrip') {
+        $('#reader img').attr('onclick', '');
+        $('#reader img').off('click', scrollToNextPage);
         $('#reader img').click(scrollToNextPage);
-        $('#reader img:last-child').attr("onclick", "window.location=nextChapter.replace('chapter','read')+'#top'");
+        $('#reader img:last-child').attr('onclick', "window.location=nextChapter.replace('chapter','read')+'#top'");
     }
     // Wenn 404, dann nächste Kapitel nicht verfügbar (Sprung direkt in nächste Kapitel) -> gehe zurück auf Zwichenseite
     if ($('#main img[src="/images/misc/404.png"]').length) {
         window.location.pathname = window.location.pathname.replace('read', 'chapter');
     }
     // Mauszeiger wird auch nur über Bild zur Hand
-    $('#reader img').addClass("pointer");
-    $('#reader a').addClass("cursorAuto");
+    $('#reader img').addClass('pointer');
+    $('#reader a').addClass('cursorAuto');
 }
 function scrollToNextPage() {
+    // @ts-ignore
+    var image = $('#chapterImage' + current_page).offset();
     $('body,html').animate({
         // nicht page+1, da id bei 0 los zählt
-        scrollTop: $('#chapterImage' + (current_page)).offset().top
+        scrollTop: image ? image.top : 0
     }, 800);
+    // @ts-ignore
     current_page = current_page + 1;
 }
 // Muster (Proxer Essentials Framework Example)
@@ -361,10 +366,10 @@ function anotherExampleMethod() {
 // Grid-Anzeige als Standard, statt Listenansicht
 // setzt "Ja ich bin Erwachsen"
 pefModulList.push({
-    id: "smallWonders",
-    name: "Kleine Wunder",
-    description: "Kleine Änderungen, die Wunder wirken",
-    autor: "Blue.Reaper",
+    id: 'smallWonders',
+    name: 'Kleine Wunder',
+    description: 'Kleine Änderungen, die Wunder wirken',
+    autor: 'Blue.Reaper',
     callMethod: function (change) { return smallWondersCall(change); }
 });
 function smallWondersCall(change) {
@@ -386,7 +391,7 @@ function smallWonders() {
     // Keine Erwachenen-Meldung mehr
     setCookie('adult', '1');
     // Cookie setzt Grid-Anzeige als Standard (im Gegensatz zu der Listenansicht), wenn noch kein Cookie gesetzt ist
-    if (getCookie("manga_reader") != "tablelist") {
+    if (getCookie('manga_reader') != 'tablelist') {
         setCookie('entryView', 'grid');
     }
     // ############### BackToTop ###############
@@ -394,10 +399,11 @@ function smallWonders() {
     if (!$('.backToTop').length) {
         // button einfügen
         var backToTopButton_1 = $('<i class="backToTop pointer fa fa-2x fa-chevron-up"/>');
-        $("body").append(backToTopButton_1);
+        $('body').append(backToTopButton_1);
         // scroll 1000 Pixel
         $(window).scroll(function () {
-            if ($(window).scrollTop() > 1000) {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop && scrollTop > 1000) {
                 backToTopButton_1.fadeIn();
             }
             else {
@@ -461,48 +467,6 @@ function theatermodusOff() {
     // Entferne Button wieder
     $('.toggleTheater').remove();
 }
-// Theme can't be activatet with modules, because it must be loaded before the content is shown
-// Init Memory
-if (GM_getValue("ModernDarkStatus") == null) {
-    GM_setValue("ModernDarkStatus", "on");
-}
-// add Theme
-if (GM_getValue("ModernDarkStatus") == "on") {
-    // Add Style after <head> to override css of side (and dont need !important everywhere)
-    // But add Before sth is shown to the user
-    $("html").append($('<style type="text/css">' + GM_getResourceText("modernDark_CSS") + '</style>'));
-}
-// Wird benötigt, da z.B Firefox nicht alle CSS Funktionen unterstützt
-function supportDesign() {
-    if (GM_getValue("ModernDarkStatus") == "on") {
-        // Bilder ersetzen
-        $('[src~="/images/status/abgeschlossen.png"]').attr('src', 'https://logosart.de/proxer2-0/abgeschlossen.png').addClass('smallImg');
-        $('[src~="/images/status/airing.png"]').attr('src', 'https://logosart.de/proxer2-0/airing.png').addClass('smallImg');
-        $('[src~="/images/status/abgebrochen.png"]').attr('src', 'https://logosart.de/proxer2-0/abgebrochen.png').addClass('smallImg');
-        $('[src~="/components/com_comprofiler/images/updateprofile.gif"]').attr('src', 'https://logosart.de/proxer2-0/edit.png');
-        $('[src~="/images/misc/onlineicon.png"]').attr('src', 'https://logosart.de/proxer2-0/abgeschlossen.png');
-        $('[src~="/images/misc/offlineicon.png"]').attr('src', 'https://logosart.de/proxer2-0/abgebrochen.png');
-        $('[src~="/images/misc/haken.png"]').attr('src', 'https://logosart.de/proxer2-0/check.png');
-        $('[src~="/images/misc/kreuz.png"]').attr('src', 'https://logosart.de/proxer2-0/cross.png');
-        $('[src~="/images/misc/stern.png"]').attr('src', 'https://logosart.de/proxer2-0/star.png').addClass('smallImg');
-        $('[src~="/images/misc/stern_grau.png"]').attr('src', 'https://logosart.de/proxer2-0/star_empty.png').addClass('smallImg');
-        $('[src~="//cdn.proxer.me/cover/894.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-anime.jpg');
-        $('[src~="//cdn.proxer.me/cover/2373.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-manga.jpg');
-        $('[src~="//cdn.proxer.me/cover/2274.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-anime.jpg');
-        $('[src~="//cdn.proxer.me/cover/2275.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-anime.jpg');
-        $('[src~="/images/misc/upload.png"]').attr('src', 'https://logosart.de/proxer2-0/upload.jpg').addClass('borderRadius6');
-        $('[src~="/images/misc/play.png"]').attr('src', 'https://logosart.de/proxer2-0/play.jpg').addClass('borderRadius6');
-        $('[src~="/images/misc/info-icon.png"]').attr('src', 'https://logosart.de/proxer2-0/info.jpg').addClass('borderRadius6');
-        $('[src~="https://proxer.me/images/misc/proxerfanpage.png"]').attr('src', 'https://logosart.de/proxer2-0/proxerfanpage.png');
-        $('[src~="/images/misc/proxerdonate.png"]').attr('src', 'https://logosart.de/proxer2-0/proxerdonate.png');
-        $('[src~="/images/misc/proxeramazon.png"]').attr('src', 'https://logosart.de/proxer2-0/proxeramazon.png');
-        $('[src~="/images/social/facebook.png"]').attr('src', 'https://logosart.de/proxer2-0/facebook.png');
-        $('[src~="/images/social/twitter.png"]').attr('src', 'https://logosart.de/proxer2-0/twitter.png');
-        $('[src~="/images/social/youtube2.png"]').attr('src', 'https://logosart.de/proxer2-0/youtube.png');
-        $('[src~="/images/social/google-plus.png"]').attr('src', 'https://logosart.de/proxer2-0/gplus.png');
-        $('[src~="/images/social/amazon.png"]').attr('src', 'https://logosart.de/proxer2-0/amazon.png');
-    }
-}
 // Zeigt Bilder in den Listenansichten an
 // IDEA use same Cookie than grid-list and add Buttons to switch List -> Grid
 // Not Ready for v4.4
@@ -565,5 +529,47 @@ function picList() {
         temp.each(function (idx, status) {
             $('.infocell').eq(idx).css("border-top-color", $(status).css("border-top-color"));
         });
+    }
+}
+// Theme can't be activatet with modules, because it must be loaded before the content is shown
+// Init Memory
+if (GM_getValue("ModernDarkStatus") == null) {
+    GM_setValue("ModernDarkStatus", "on");
+}
+// add Theme
+if (GM_getValue("ModernDarkStatus") == "on") {
+    // Add Style after <head> to override css of side (and dont need !important everywhere)
+    // But add Before sth is shown to the user
+    $("html").append($('<style type="text/css">' + GM_getResourceText("modernDark_CSS") + '</style>'));
+}
+// Wird benötigt, da z.B Firefox nicht alle CSS Funktionen unterstützt
+function supportDesign() {
+    if (GM_getValue("ModernDarkStatus") == "on") {
+        // Bilder ersetzen
+        $('[src~="/images/status/abgeschlossen.png"]').attr('src', 'https://logosart.de/proxer2-0/abgeschlossen.png').addClass('smallImg');
+        $('[src~="/images/status/airing.png"]').attr('src', 'https://logosart.de/proxer2-0/airing.png').addClass('smallImg');
+        $('[src~="/images/status/abgebrochen.png"]').attr('src', 'https://logosart.de/proxer2-0/abgebrochen.png').addClass('smallImg');
+        $('[src~="/components/com_comprofiler/images/updateprofile.gif"]').attr('src', 'https://logosart.de/proxer2-0/edit.png');
+        $('[src~="/images/misc/onlineicon.png"]').attr('src', 'https://logosart.de/proxer2-0/abgeschlossen.png');
+        $('[src~="/images/misc/offlineicon.png"]').attr('src', 'https://logosart.de/proxer2-0/abgebrochen.png');
+        $('[src~="/images/misc/haken.png"]').attr('src', 'https://logosart.de/proxer2-0/check.png');
+        $('[src~="/images/misc/kreuz.png"]').attr('src', 'https://logosart.de/proxer2-0/cross.png');
+        $('[src~="/images/misc/stern.png"]').attr('src', 'https://logosart.de/proxer2-0/star.png').addClass('smallImg');
+        $('[src~="/images/misc/stern_grau.png"]').attr('src', 'https://logosart.de/proxer2-0/star_empty.png').addClass('smallImg');
+        $('[src~="//cdn.proxer.me/cover/894.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-anime.jpg');
+        $('[src~="//cdn.proxer.me/cover/2373.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-manga.jpg');
+        $('[src~="//cdn.proxer.me/cover/2274.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-anime.jpg');
+        $('[src~="//cdn.proxer.me/cover/2275.jpg"]').attr('src', 'https://logosart.de/proxer2-0/proxer-test-anime.jpg');
+        $('[src~="/images/misc/upload.png"]').attr('src', 'https://logosart.de/proxer2-0/upload.jpg').addClass('borderRadius6');
+        $('[src~="/images/misc/play.png"]').attr('src', 'https://logosart.de/proxer2-0/play.jpg').addClass('borderRadius6');
+        $('[src~="/images/misc/info-icon.png"]').attr('src', 'https://logosart.de/proxer2-0/info.jpg').addClass('borderRadius6');
+        $('[src~="https://proxer.me/images/misc/proxerfanpage.png"]').attr('src', 'https://logosart.de/proxer2-0/proxerfanpage.png');
+        $('[src~="/images/misc/proxerdonate.png"]').attr('src', 'https://logosart.de/proxer2-0/proxerdonate.png');
+        $('[src~="/images/misc/proxeramazon.png"]').attr('src', 'https://logosart.de/proxer2-0/proxeramazon.png');
+        $('[src~="/images/social/facebook.png"]').attr('src', 'https://logosart.de/proxer2-0/facebook.png');
+        $('[src~="/images/social/twitter.png"]').attr('src', 'https://logosart.de/proxer2-0/twitter.png');
+        $('[src~="/images/social/youtube2.png"]').attr('src', 'https://logosart.de/proxer2-0/youtube.png');
+        $('[src~="/images/social/google-plus.png"]').attr('src', 'https://logosart.de/proxer2-0/gplus.png');
+        $('[src~="/images/social/amazon.png"]').attr('src', 'https://logosart.de/proxer2-0/amazon.png');
     }
 }
