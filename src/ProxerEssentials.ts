02167b5a9c0d53f7d82ccae3e9704b84b13b4d30
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2019-04-30 23:20:06 +0200

    add @hompate, @support, etc.

--- src/ProxerEssentials.ts
+++ src/ProxerEssentials.ts
@@ -4,7 +4,13 @@
 // @description Nützlicher Erweiterungen für Proxer die jeder braucht
 // @author      Blue.Reaper
 // @namespace   https://blue-reaper.github.io/Proxer-Essentials/
+// @homepage    https://blue-reaper.github.io/Proxer-Essentials/
+// @supportURL  https://github.com/Blue-Reaper/Proxer-Essentials/issues/new/choose
+// @icon        https://raw.githubusercontent.com/Blue-Reaper/Proxer-Essentials/master/src/framework/img/logo_proxer.png
+// @updateURL   https://greasyfork.org/scripts/382408-proxer-essentials/code/Proxer%20Essentials.user.js
+// @downloadURL https://greasyfork.org/scripts/382408-proxer-essentials/code/Proxer%20Essentials.user.js
 // @include     https://proxer.me/*
+// @require     http://code.jquery.com/jquery-3.4.0.min.js
 // @run-at      document-start
 // @grant       GM_setValue
 // @grant       GM_getValue
@@ -14,9 +20,6 @@
 // @grant       GM_getResourceURL
 // Konsolenausgabe für Debugging
 // @grant       GM_log
-// @updateURL https://greasyfork.org/scripts/382408-proxer-essentials/code/Proxer%20Essentials.user.js
-// @downloadURL https://greasyfork.org/scripts/382408-proxer-essentials/code/Proxer%20Essentials.user.js
-// @require     http://code.jquery.com/jquery-3.4.0.min.js
 // @resource    framework_CSS      https://raw.githubusercontent.com/Blue-Reaper/Proxer-Essentials/dev/src/framework/css/framework.css
 // @resource    modules_CSS        https://raw.githubusercontent.com/Blue-Reaper/Proxer-Essentials/dev/src/modules/css/modules.css
 // @resource    design_CSS         https://raw.githubusercontent.com/Blue-Reaper/Proxer-Essentials/dev/src/framework/css/design.css
