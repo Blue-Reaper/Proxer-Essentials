de058394c3140f0855d6d7dc484e61eec8f8c6f9
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2019-05-01 13:50:25 +0200

    try new @downloadURL

--- ProxerEssentials.user.js
+++ ProxerEssentials.user.js
@@ -9,7 +9,7 @@
 // @supportURL  https://github.com/Blue-Reaper/Proxer-Essentials/issues/new/choose
 // @icon        https://raw.githubusercontent.com/Blue-Reaper/Proxer-Essentials/master/src/framework/img/logo_proxer.png
 // @updateURL https://openuserjs.org/meta/Blue.Reaper/Proxer_Essentials.meta.js
-// @updateURL https://openuserjs.org/install/Blue.Reaper/Proxer_Essentials.user.js
+// @downloadURL https://openuserjs.org/install/Blue.Reaper/Proxer_Essentials.user.js
 // @license MIT
 // @include     https://proxer.me/*
 // @require     http://code.jquery.com/jquery-3.4.0.min.js
