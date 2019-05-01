79dfe9fba5f384651514ba62ca2895bbb8a5bbeb
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2019-05-01 13:58:58 +0200

    update metha-block

--- src/ProxerEssentials.ts
+++ src/ProxerEssentials.ts
@@ -7,9 +7,10 @@
 // @homepage    https://blue-reaper.github.io/Proxer-Essentials/
 // @supportURL  https://github.com/Blue-Reaper/Proxer-Essentials/issues/new/choose
 // @icon        https://raw.githubusercontent.com/Blue-Reaper/Proxer-Essentials/master/src/framework/img/logo_proxer.png
-// @updateURL https://openuserjs.org/meta/Blue.Reaper/Proxer_Essentials.meta.js
+// @updateURL   https://openuserjs.org/meta/Blue.Reaper/Proxer_Essentials.meta.js
 // @downloadURL https://openuserjs.org/install/Blue.Reaper/Proxer_Essentials.user.js
-// @license MIT
+// @license     MIT
+// @copyright   2019, Blue.Reaper (https://blue-reaper.github.io/Proxer-Essentials/)
 // @include     https://proxer.me/*
 // @require     http://code.jquery.com/jquery-3.4.0.min.js
 // @run-at      document-start
@@ -30,6 +31,10 @@
 // @include     https://stream.proxer.me/*
 // @resource    theater_CSS        https://raw.githubusercontent.com/Blue-Reaper/Proxer-Essentials/dev/src/modules/css/theaterModus.css
 // ==/UserScript==
+
+// ==OpenUserJS==
+// @author Blue.Reaper
+// ==/OpenUserJS==
 
 GM_addStyle (GM_getResourceText ("framework_CSS"));
 GM_addStyle (GM_getResourceText ("modules_CSS"));
