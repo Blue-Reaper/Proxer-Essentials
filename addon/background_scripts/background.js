e218a7bd63182a09a7c439280f693bb47e35153c
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2019-10-03 04:58:05 +0200

    add features from modul MangaComfort + SmallWonders

--- addon/background_scripts/background.js
+++ addon/background_scripts/background.js
@@ -2,8 +2,8 @@ let isCookieSet = false;
 
 // TEMP for testing
 browser.browserAction.onClicked.addListener(()=>{
-    browser.tabs.removeCSS({file: "addon/css/root.css"});
-    browser.tabs.removeCSS({file: "src/framework/css/design.css"});
+    browser.tabs.removeCSS({file: "addon/css/essentials.css"});
+    browser.tabs.removeCSS({file: "addon/css/design.css"});
 });
 
 // TODO when use which event trigger?
@@ -12,21 +12,61 @@ browser.tabs.onUpdated.addListener(tapUpdate);
 // update when the tab is activated
 browser.tabs.onActivated.addListener(tapUpdate);
 
+
+
+// Listen for messages from content_scripts
+browser.runtime.onMessage.addListener(handleMessage);
+
+function handleMessage(request, sender, sendResponse) {
+
+  // if (sender.url != browser.runtime.getURL("/devtools/panel/panel.html")) {
+  //   return;
+  // }
+     if (request.MangaBack){
+         var newUrl = sender.url.replace('read', 'chapter');
+         browser.tabs.update(sender.tab.id,{url: newUrl});
+     }
+}
+
 function getActiveTab() {
   return browser.tabs.query({active: true, currentWindow: true});
 }
 
 function tapUpdate() {
+    setCSS();
+    setCookie();
+
     getActiveTab().then((tabs) => {
-        setCSS();
-        setCookie();
-        // browser.tabs.sendMessage(tabs[0].id, {image: cookieVal.image});
+        checkMangaReader(tabs[0]);
+
         // browser.tabs.sendMessage(tabs[0].id, {color: cookieVal.color});
     });
 }
 
+async function checkMangaReader(activeTab){
+    let urlpart = activeTab.url.split('/')[3];
+    if (urlpart == 'read' || urlpart == 'chapter'){
+        let getCookieManga = browser.cookies.get({
+            url: "https://proxer.me/",
+            name: "manga_reader"
+        });
+        getCookieManga.then((cookie) => {
+          if (cookie.value == "longstrip") {
+              browser.tabs.sendMessage(activeTab.id, {mangaReader: "On"});
+          } else {
+              browser.tabs.sendMessage(activeTab.id, {mangaReader: "Off"});
+          }
+        });
+    } else {
+        browser.tabs.sendMessage(activeTab.id, {mangaReader: "Off"});
+    }
+}
+
 async function setCSS(){
+// optional css (currently not used)
+    if(false){
         browser.tabs.insertCSS({runAt: "document_start", file: "src/modules/css/smallWonders.css"});
+    }
 }
 
 async function setCookie(){
--- addon/content_scripts/easyNavigation.js
+++ addon/content_scripts/easyNavigation.js
@@ -1,8 +1,8 @@
+browser.runtime.onMessage.addListener(mangaReader);
 easyNavigation();
-async function easyNavigation() {
-    console.log("navigation");
-    console.log($('body').length);
+let updating = false;
 
+async function easyNavigation() {
     // BackToTop
     if (!$('.backToTop').length) {
         // add button
@@ -28,5 +28,76 @@ async function easyNavigation() {
     if(!$('#leftNav li:nth-child(3) ul li>a[href="/manga/updates#top').length){
         $('#leftNav li:nth-child(3) ul').append($('<li><a data-ajax="true" href="/manga/updates#top">Updates</a></li>'));
     }
+}
 
+function mangaReader(request, sender, sendResponse) {
+    if (request.mangaReader == "On") {
+        easyNavigationManga();
+    } else if (request.mangaReader == "Off") {
+        removeeasyNavigationManga();
+    }
+}
+
+async function easyNavigationManga() {
+    // IDEA: able to "scroll" by grabbing with courser, or zoom with click
+    // remove jumping to next chapter with click on image
+    $('#reader img').attr('onclick', '');
+    // only add buttons once
+    if(!$('.nextChapter').length){
+        // button previous chapter
+        // don't show button on first chapter
+        if(Number(location.pathname.split('/')[3]) > 1){
+            let previousChapterButton = $('<i class="previousChapter pointer fa fa-2x fa-chevron-left"/>');
+            $('body').append(previousChapterButton);
+            previousChapterButton.click(() => {
+                let path = location.pathname.split('/');
+                path[1] = 'read';
+                path[3] = String(Number(path[3])-1);
+                path[5] = String(1);
+                location.pathname = path.join('/');
+            });
+        }
+        // button next chapter
+        let nextChapterButton = $('<i class="nextChapter pointer fa fa-2x fa-chevron-right"/>');
+        $('body').append(nextChapterButton);
+        nextChapterButton.click(() => {
+            let path = location.pathname.split('/');
+            path[1] = 'read';
+            path[3] = String(Number(path[3])+1);
+            path[5] = String(1);
+            location.pathname = path.join('/');
+        });
+        // button bookmark this chapter
+        let bookmarkButton = $('<i class="bookmark pointer fa fa-2x fa-bookmark"/>');
+        $('body').append(bookmarkButton);
+        bookmarkButton.click(() => {
+            let path = location;
+            path = String(path).replace('read', 'chapter');
+            let ajaxLink = (path + '?format=json&type=reminder&' + $('#proxerToken').val() + '=1&title=reminder_this');
+            $.post(ajaxLink, {
+                'check': 1
+            }, function(data) {
+                createPefMessage(data.msg);
+                localStorage.listentries_timer = null;
+            });
+        });
+    }
+
+    // Wenn 404, dann nächste Kapitel nicht verfügbar (Sprung direkt in nächste Kapitel) -> gehe zurück auf Zwichenseite
+    if ($('#main img[src="/images/misc/404.png"]').length) {
+        if(!updating){
+            updating = true;
+            browser.runtime.sendMessage("ProxerEssentials@blue-reaper.github.io",{MangaBack:1});
+        }
+    }
+
+    // Mauszeiger wird auch nur über Bild zur Hand
+    $('#reader img').addClass('pointer');
+    $('#reader a').addClass('cursorAuto');
+
+}
+
+// remove Manga Navigation outside of Reader
+async function removeeasyNavigationManga(){
+    $('.previousChapter, .nextChapter, .bookmark').remove();
 }
