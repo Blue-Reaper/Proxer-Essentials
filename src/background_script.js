a80fd5e8fc0ecffb9163177b9afa42e2be8413d7
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2020-12-24 13:52:23 +0100

    try proxer api

--- src/background_script.js
+++ src/background_script.js
@@ -1,1 +1,9 @@
 // Put all the javascript code here, that you want to execute in background.
+
+$.post('https://proxer.me/api/v1/user/login').done((data) => {
+  // todo warten auf proxer api key
+  console.log('response error ' + data.error);
+  console.log('response message ' + data.message);
+  console.log('response code ' + data.code);
+  console.log('response data ' + data.data);
+});
